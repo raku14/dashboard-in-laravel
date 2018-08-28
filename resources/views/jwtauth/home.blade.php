a3b3c3ec2f89febde58499cf508f9012e11a2573
raku14 <sacrawat22@gmail.com>
2018-08-28 18:32:41 +0530

    woking on post

--- resources/views/jwtauth/home.blade.php
+++ resources/views/jwtauth/home.blade.php
@@ -11,10 +11,29 @@
                     <img class="img-thumbnail" src="/profile/{{session('photo') != '' ? session('photo') : 'user.png' }}" style="height: 250px; width: 250px; border: 3px double skyblue;">
                    
                 </div>
-            </div>
-        </div>
+                <div class="col-md-6" style="background: white;">
+                    <form class="form-horizotal">
+                        <div class="form-group">
+                            <label class="control-label"><b>Title :</b> </label>
+                           <div class="form-group">
+                                <input type="text" name="title" class="form-control">
+                           </div>
+                        </div>
+                        <div class="form-group">
+                            <textarea rows="4" class="form-control" >Write something here...</textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Post" class="btn btn-info">
+                        </div>
+                    </form>
+                </div>
 
+            </div>
+
+        </div>
+   
     </div>
+     <hr>
 </div>
 <script type="text/javascript">
     jQuery(document).ready(function($){
